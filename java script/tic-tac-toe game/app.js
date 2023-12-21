366b8f362b975be5943e65920a6e00f150da0ebe
pritesh <senjaliyapritesh37@gmail.com>
2023-12-21 10:48:35 +0530

    done

---
+++ java script/tic-tac-toe game/app.js
@@ -0,0 +1,2 @@
+let boxes=document.querySelectorAll(".box")
+let btn=document.querySelector("#reset_btn")
---
+++ real me/script.js
@@ -0,0 +1,6 @@
+let color1 = document.getElementById(color);
+function change(){
+    color1.style.color="red"
+//  alert("hello");
+}
+console.log(color1);
